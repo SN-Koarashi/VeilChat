be298acc1571cd568d1481e629620889c992d16a
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-01-04 23:33:44 +0800

    更新版本 v4.0.0

--- development/run.js
+++ development/run.js
@@ -2,63 +2,46 @@
 const request = require('request');
 const path = require('path');
 const fs = require('fs-extra');
-const { JSFuck } = require('./jsfuck.js');
 const JSOF = require('javascript-obfuscator');
 const UglifyJS = require("uglify-js");
 
 var result;
-var isMinify = false;
 var isDeploy = false;
-var isEncrypt = false;
-console.log("[INFO]","開始編譯", __dirname);
+console.log("[INFO]", "開始編譯", __dirname);
 
 process.argv.forEach(function (val, index, array) {
-  console.log("[INFO]","獲取參數" ,index + ': ' + val);
-  
-  if(val == "--minify") isMinify = true;
-  if(val == "--deploy") isDeploy = true;
-  if(val == "--encrypt") isEncrypt = true;
+	console.log("[INFO]", "獲取參數", index + ': ' + val);
+
+	if (val == "--deploy") isDeploy = true;
 });
 console.log("---");
-const inputData = fs.readFileSync(path.join(__dirname, "bin", "main.bundle.js"),{encoding:'utf8', flag:'r'});
+const inputData = fs.readFileSync(path.join(__dirname, "bin", "main.bundle.js"), { encoding: 'utf8', flag: 'r' });
 
-if(!isMinify && !isDeploy && !isEncrypt){
-	console.log("[INFO]","執行程式碼部署");
-	fs.writeFileSync(path.join(__dirname, "../public/js", "main.encrypted.js"),inputData);
-	console.log("[INFO]","程式碼部署完成");
-	
-	return;
-}
 
-console.log("[INFO]","執行最小化作業");
-result = UglifyJS.minify(inputData, {output:{preamble: `/*! 
+console.log("[INFO]", "執行最小化作業");
+result = UglifyJS.minify(inputData, {
+	output: {
+		preamble: `/*! 
  * Veil WebSocket WebChat
  * Powered by SNKms.com
  * Copyright 2023 Sky-Night Kamhu Mitor Seuna
  * Release date by ${new Date().toLocaleString('zh-TW', {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-  timeZoneName: 'shortOffset',
-  hour12: false
-})}
-*/`}});
+			year: 'numeric',
+			month: 'numeric',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZoneName: 'shortOffset',
+			hour12: false
+		})}
+*/`}
+});
 fs.writeFileSync(path.join(__dirname, "bin", "main.temp.js"), result.code);
-console.log("[INFO]","最小化完成");
+console.log("[INFO]", "最小化完成");
 
-if(isMinify){
-	console.log("[INFO]","執行程式碼部署");
-	fs.writeFileSync(path.join(__dirname, "../public/js", "main.encrypted.js"),result.code);
-	console.log("[INFO]","程式碼部署完成");
-	
-	return;
-}
-
-console.log("[INFO]","執行混淆作業");
-var result = JSOF.obfuscate(result.code,
+console.log("[INFO]", "執行混淆作業");
+result = JSOF.obfuscate(result.code,
 	{
 		target: 'browser-no-eval',
 		selfDefending: false,
@@ -76,48 +59,34 @@ var result = JSOF.obfuscate(result.code,
 );
 var resultCode = result.getObfuscatedCode();
 
-console.log("[INFO]","混淆完成");
+console.log("[INFO]", "混淆完成");
 fs.writeFileSync(path.join(__dirname, "bin", "main.obfuscate.js"), resultCode);
-result = UglifyJS.minify(resultCode, {output:{preamble: `/*! 
+result = UglifyJS.minify(resultCode, {
+	output: {
+		preamble: `/*! 
  * Veil WebSocket WebChat
  * Powered by SNKms.com
  * Copyright 2023 Sky-Night Kamhu Mitor Seuna
  * Release date by ${new Date().toLocaleString('zh-TW', {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-  timeZoneName: 'shortOffset',
-  hour12: false
-})}
-*/`}});
+			year: 'numeric',
+			month: 'numeric',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric',
+			timeZoneName: 'shortOffset',
+			hour12: false
+		})}
+*/`}
+});
 fs.writeFileSync(path.join(__dirname, "bin", "main.min.js"), result.code);
 
-//var result = {};
-//result.code = `/*! 
-// * Veil WebSocket WebChat
-// * Powered by SNKms.com
-// * Copyright 2023 Sky-Night Kamhu Mitor Seuna
-//*/` + resultCode;
-//fs.writeFileSync(path.join(__dirname, "bin", "main.min.js"), result.code);
+console.log("[INFO]", "混淆最小化完成");
 
-console.log("[INFO]","混淆最小化完成");
+var encrypted = result.code;
 
-var encrypted;
-if(isEncrypt){
-	console.log("[INFO]","執行加密作業");
-	encrypted = JSFuck.encode(result.code, true, true);
-	fs.writeFileSync(path.join(__dirname, "bin", "main.encrypted.js"),encrypted);
-	console.log("[INFO]","加密完成");
-}
-else{
-	encrypted = result.code;
-}
-
-if(isDeploy){
-	console.log("[INFO]","執行程式碼部署");
-	fs.writeFileSync(path.join(__dirname, "../public/js", "main.encrypted.js"),encrypted);
-	console.log("[INFO]","程式碼部署完成");
+if (isDeploy) {
+	console.log("[INFO]", "執行程式碼部署");
+	fs.writeFileSync(path.join(__dirname, "../public/js", "main.encrypted.js"), encrypted);
+	console.log("[INFO]", "程式碼部署完成");
 }
