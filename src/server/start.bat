d4882d92ca6d3af6ce4edff0070220c9f21d306f
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-01-17 03:37:20 +0800

    刪除 bat

--- src/server/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Veil WebChat Server
-echo     Started at %date% %time%
-echo  Veil WebSocket WebChat Server Side Worker
-echo -----------------------------------------------
-node ./src/server.js
-pause
--- src/server/start_simulation.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Veil WebChat Server [Debug]
-echo     Started at %date% %time%
-echo  Veil WebSocket WebChat Server Side Worker
-echo -----------------------------------------------
-node ./src/server.js --debug
-pause
