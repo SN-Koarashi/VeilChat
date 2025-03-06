949b5b05eb50de0fc53cf33d0f3ce6c728636b05
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-03-06 23:47:28 +0800

    更新訊息

--- src/client/src/WebSocket/Open.js
+++ src/client/src/WebSocket/Open.js
@@ -2,18 +2,17 @@
 
 import config from '../config.js';
 import Logger from '../Functions/Logger.js';
-import Dialog from '../Functions/Dialog.js'
 import { onScroll } from '../Utils/ChatUtils.js';
 import { isMobile } from '../Utils/Utils.js';
 import { WebSocketBinaryHandler } from '../Registers/WebSocket.js';
 
 export default function RegisterEvent() {
     Logger.show(Logger.Types.LOG, '[WebSocketHandler]', { type: "init", reason: "server connected" });
-    let toast = "與伺服器連線成功";
-    if (isMobile())
-        Dialog.toastMessage(toast, 'signal_cellular_alt', 'green');
-    else
-        Dialog.success(toast);
+    // let toast = "與伺服器連線成功";
+    // if (isMobile())
+    //     Dialog.toastMessage(toast, 'signal_cellular_alt', 'green');
+    // else
+    //     Dialog.success(toast);
 
     let joinLocation = location.pathname.replace(/\/$/, '').split('/').at(-1);
     if (isMobile() && config.localStorage.getItem('lastRoom') && location.pathname.match(/^\/p\/([0-9A-Za-z\-_]+)/ig) === null) {
